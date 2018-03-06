a121047e35e627e5c51dcde98dc52052fe0f358a
Magagula Sifiso <magagjs@unisa.ac.za>
2018-03-06 12:55:09 +0200

    Add load button in gallery page and decrease number of image initially displayed to 12. Load button will load more images for Instagram feed.

--- js/mytCustom.js
+++ js/mytCustom.js
@@ -1,17 +1,36 @@
-$( document ).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
 	
 	var userFeed = new Instafeed({
 	    get: 'user',
 	    userId: '3991179505',
-	    limit: '35',
+	    limit: '12',
 	    sortBy: 'most-recent',
 	    resolution: 'standard_resolution',
 	    clientId: 'c400f2940c624504a67b87ad2159d976',
 		accessToken: '3991179505.1677ed0.bb3a32b98f384983bfaac7340fec426e',
-		template: '<div class="col-lg-3 myt-gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" keywords=""class="img-responsive"/></a></div>'
+		target:  'instafeed',
+		template: '<div class="col-lg-3 myt-gallery"><a href="{{image}}" title="{{caption}}" target="_blank">'+
+				  '<img src="{{image}}" alt="{{caption}}" keywords=""class="img-responsive"/></a>'+
+				  '<p>{{caption}}</p></div>',
+		
+		// disable feed load button when feed has no more images
+		after: function () {
+			instaLoadButton.stye.display = 'none';
+			if(!this.hasNext()) {
+				instaLoadButton.setAttribute('disabled', 'disabled');	
+			}
+		}
 	});
-	userFeed.run();
 	
+	var instaLoadButton = document.getElementById("load-feed-btn"),	// get button to load more feeds by ID
+		instLoadDiv = document.getElementById("instafeed");			// get the <div> container of feed images by ID
+	
+	instaLoadButton.addEventListener("click", function() {			// function to load next feed images
+		userFeed.next();
+	})
+	
+	userFeed.run();
+
 	// This will create a single gallery from all elements that have class "myt-gallery"
 	$('.myt-gallery').magnificPopup({
 	  type: 'image',
