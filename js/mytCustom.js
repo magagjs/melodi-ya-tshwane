49ef52b0f6728af0d617cc0a25d047b1e5223e43
Magagula Sifiso <magagjs@unisa.ac.za>
2018-03-06 04:26:05 +0200

    Removed lightwidget from Grallery and added custom Gallery to fetch Instagram feed using instafeed.js. Added styling to gallery and used magnific-popup.js to create boxed photo album

---
+++ js/mytCustom.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+	
+	var userFeed = new Instafeed({
+	    get: 'user',
+	    userId: '3991179505',
+	    limit: '35',
+	    sortBy: 'most-recent',
+	    resolution: 'standard_resolution',
+	    clientId: 'c400f2940c624504a67b87ad2159d976'
+		accessToken: '3991179505.1677ed0.bb3a32b98f384983bfaac7340fec426e',
+		template: '<div class="col-lg-3 myt-gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" keywords=""class="img-responsive"/></a></div>'
+	});
+	userFeed.run();
+	
+	// This will create a single gallery from all elements that have class "myt-gallery"
+	$('.myt-gallery').magnificPopup({
+	  type: 'image',
+	  delegate: 'a',	// target anchor <a> tag
+	  gallery:{
+	    enabled:true
+	  }
+	});
+	
+});
