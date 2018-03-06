46479a77d880e862a5649e4e6133c6fbea232b44
Magagula Sifiso <magagjs@unisa.ac.za>
2018-03-06 04:30:08 +0200

    Fixed JavaScript syntax error.

--- js/mytCustom.js
+++ js/mytCustom.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
 	    limit: '35',
 	    sortBy: 'most-recent',
 	    resolution: 'standard_resolution',
-	    clientId: 'c400f2940c624504a67b87ad2159d976'
+	    clientId: 'c400f2940c624504a67b87ad2159d976',
 		accessToken: '3991179505.1677ed0.bb3a32b98f384983bfaac7340fec426e',
 		template: '<div class="col-lg-3 myt-gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" keywords=""class="img-responsive"/></a></div>'
 	});
@@ -17,7 +17,7 @@ $( document ).ready(function() {
 	  type: 'image',
 	  delegate: 'a',	// target anchor <a> tag
 	  gallery:{
-	    enabled:true
+	  enabled:true
 	  }
 	});
 	
