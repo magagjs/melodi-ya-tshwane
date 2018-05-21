fd070547c70b95d7034dc79ed62dc3c5b48c525f
Magagula Sifiso <magagjs@unisa.ac.za>
2018-05-21 20:47:39 +0200

    Improved UI, added content and renamed images

--- js/myt-custom.js
+++ js/myt-custom.js
@@ -34,13 +34,25 @@ $(document).ready(function() {
 	$(window).scroll(function(){
 		showNavArrow();		// run function to hide/display scroll icon
 		navBarScroll();		// run function to hide/display navbar
+		
+		// restore navbar to default menu if search button is activated
+		if( $('#search-box').hasClass('search-box-display') ){
+			$('#search-box').addClass('search-box-hide')		   		 // hide search box
+				.removeClass('search-box-display'); 			   		 
+			$('#search-button').addClass('glyphicon glyphicon-search')   // change close icon back into search icon
+				.removeClass('fas fa-window-close');
+			$('.navbar-brand-hide').addClass('navbar-brand')	   		 // display myt logo link
+				.removeClass('navbar-brand-hide'); 	
+			$('.navbar-header-hide').addClass('navbar-header')	   		 // display navbar-header Menu link
+				.removeClass('navbar-header-hide'); 
+		}
 	});
 	
 	// smooth scroll to top of page when scroll icon link clicked
 	$('.scrollIcon').click(function(){
 		$("html, body").animate({ scrollTop: 0 }, 1000); 	  //perform the smooth scroll animation
 		$('nav').addClass('fall-in').removeClass('fall-out'); // display navbar with animation
-		lastScrollPos = 0;		// reset to prevent navBarScroll function from firing
+		lastScrollPos = 0;									  // reset to prevent navBarScroll function from firing
 		
 		return false;
 	});
@@ -48,16 +60,40 @@ $(document).ready(function() {
 	// change search button to search box on click
 	$('.toggle-search').click(function(){
 		if( $('#search-box').hasClass('search-box-hide') ){
-			$('#search-box').addClass('search-box-display')	// add display class
-				.removeClass('search-box-hide'); 			// remove hide class
+			$('#search-box').addClass('search-box-display')		   // display search box
+				.removeClass('search-box-hide'); 				   // remove hide class used to hide search box
+			$('#search-button').addClass('fas fa-window-close')    // change search icon into close icon
+				.removeClass('glyphicon glyphicon-search');			
+			$('.navbar-brand').addClass('navbar-brand-hide')	   // remove myt logo link - to have more horizontal space
+				.removeClass('navbar-brand'); 
+			$('.navbar-header').addClass('navbar-header-hide')	   // remove navbar-header Menu link
+				.removeClass('navbar-header'); 
 		}else{
-			$('#search-box').addClass('search-box-hide')	// add hide class
-			.removeClass('search-box-display'); 			// remove display class
+			$('#search-box').addClass('search-box-hide')		   		 // hide search box
+				.removeClass('search-box-display'); 			   		 // remove display class used to display search box
+			$('#search-button').addClass('glyphicon glyphicon-search')   // change close icon back into search icon
+				.removeClass('fas fa-window-close');
+			$('.navbar-brand-hide').addClass('navbar-brand')	   		 // display myt logo link
+				.removeClass('navbar-brand-hide'); 	
+			$('.navbar-header-hide').addClass('navbar-header')	   		 // display navbar-header Menu link
+				.removeClass('navbar-header-hide'); 			
 		}
 			
 		return false;
 	});
 	
+	// accordion to display minus icon to an element that is collapsed in panel-heading
+	$(".collapse.in").each(function(){
+		$(this).siblings(".panel-heading").find(".fas").addClass("fa-minus")
+			.removeClass("fa-plus");
+	});
+	
+	// accordion to toggle plus/minus icon to an element that is collapsed/expanded in panel-heading
+	$(".collapse").on('show.bs.collapse', function(){
+		$(this).parent().find(".fas").removeClass("fa-plus").addClass("fa-minus");})
+			.on('hide.bs.collapse', function(){
+				$(this).parent().find(".fas").removeClass("fa-minus").addClass("fa-plus");
+			});	
 });
 	
 
